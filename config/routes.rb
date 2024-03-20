3d8160acaaa067b85532450bf80865cd74c29e9e
FUMIGear <iso.fumi429@gmail.com>
2024-03-21 07:39:40 +0900

    1.5の演習２でプッシュすると表示が変わるのか

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # root "hello#index" # 元コード
-  root "application#hello" # リスト1.10
-  # root "application#goodbye" # 1.3.4の演習3
+  # root "application#hello" # リスト1.10
+  root "application#goodbye" # 1.3.4の演習3
 end
