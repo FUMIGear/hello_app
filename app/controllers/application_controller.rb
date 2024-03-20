e39e5183ec02228b5a2ce9b77b086f5d7ed79e55
FUMIGear <iso.fumi429@gmail.com>
2024-03-20 17:40:00 +0900

    チュートリアル1.4.1の途中まで実施しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # メソッド：リスト1.9
+  def hello
+    # render html: "hello, world!"
+    # render html: "hola, mundo!" # 1.3.4の演習1
+    render html: "¡Hola, mundo" # 1.3.4の演習2
+  end
+  # メソッド：1.3.4の演習3
+  def goodbye
+    render html: "goodbye, world!"
+  end
 end
