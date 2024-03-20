8bc31095cd7560996285464e7f63006b1c954b52
FUMIGear <iso.fumi429@gmail.com>
2024-03-21 07:25:25 +0900

    1.5の演習１でプッシュすることで表示が変わるか確認中。パート２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   # メソッド：リスト1.9
   def hello
-    # render html: "hello, world!"
+    render html: "hello, world!"
     # render html: "hola, mundo!" # 1.3.4の演習1
-    render html: "¡Hola, mundo" # 1.3.4の演習2
+    # render html: "¡Hola, mundo" # 1.3.4の演習2
   end
   # メソッド：1.3.4の演習3
   def goodbye
     # render html: "goodbye, world!"
-    render html: "hola, mundo!" # 1.3.4の演習1
   end
 end
